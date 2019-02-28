ec83ae31a8e61683e1455b3d6d48e2ee745cb74f
Alvaro Velasco <02alvaro.velasco@gulllakecs.org>
2019-02-28 09:25:35 -0500

    Added a repr and eq method to my class and printed some values to demonstrate it

--- 8.1.5: The Rectangle Class.py
+++ 8.1.5: The Rectangle Class.py
@@ -1,15 +1,31 @@
 class Rectangle:
     def __init__(self, x=0, y=0):
-        Rectangle.length = x
-        Rectangle.width = y
+        self.length = x
+        self.width = y
     def get_area (self):
         return self.length * self.width
 
     def get_perimeter (self):
         return (self.length * 2) + (self.width * 2)
 
+    def __repr__(self):
+        return "Lenght: " + str(self.length) + " Width: " + str(self.width)
+
+    def __eq__(self, other):
+        return self.width == other.width and self.length == other.length
+
+#dddd
+
+
 r = Rectangle(6,8)
-print ("Lenght: " + str(r.length))
-print ("Width: " + str(r.width))
+r1 = Rectangle(5,9)
+r2 = Rectangle(6,8)
+print("Lenght: " + str(r.length))
+print("Width: " + str(r.width))
 print("Area: " + str(r.get_area()))
 print("Perimeter: " + str(r.get_perimeter()))
+print("r-> " + str(r))
+print("r1-> " + str(r1))
+print("r2-> " + str(r2))
+print(r == r1)
+print(r == r2)
