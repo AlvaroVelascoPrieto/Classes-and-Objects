f296d51d60d5e84b36c923c7ba8c1d276360a2a1
Alvaro Velasco <02alvaro.velasco@gulllakecs.org>
2019-03-06 10:10:49 -0500

    Added the repr method, working properly

--- 8.3.8 - Names in a hat.py
+++ 8.3.8 - Names in a hat.py
@@ -4,15 +4,24 @@ class Hat():
         self.name = name
 
     def insert_name(self):
+        global namelist
         namelist = []
         how_many_names = int(input("How many names do you want to enter? "))
         for i in range(how_many_names):
             newname = input("Enter name: ")
-            for i, each in enumerate(namelist):
-                if newname != each:
-                    namelist = namelist + newname
-                else:
-                    continue
-    
+            if len(namelist)==0:
+                namelist.append(newname)
+            else:
+                for i, each in enumerate(namelist):
+                    if newname != each:
+                        namelist.append(newname)
+                        break
+                    else:
+                        continue
+
+    def __repr__(self, namelist):
+        print("The hat contains the following names: " + str(namelist))
+
 h = Hat(0)
 h.insert_name()
+h.__repr__(namelist)
